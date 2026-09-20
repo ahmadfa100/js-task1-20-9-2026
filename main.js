b63aed98e64f766dd88b0ebabf6c1a126487bd8a
Ahmad bani Hamad <ahmad100.fa100@gmail.com>
2026-09-20 14:38:45 +0300

    fix errors٢

--- main.js
+++ main.js
@@ -1,68 +1,44 @@
-const userName = prompt("Enter your name");
-const userAge = Number(prompt("Enter your age"));
-const userGender = prompt("Enter your gender (Male / Female)");
+const userName = prompt("Enter you name");
+const userAge = prompt("Enter you age");
+const userGender = prompt("Enter you gender");
 
-let userChoise = "No order";
+let male = true;
 let valid = false;
+let userChoise;
 
-
-if (userGender === "Male") {
-
-    document.write("<h3>Welcome Mr. " + userName + "</h3>");
-
-} else if (userGender === "Female") {
-
-    document.write("<h3>Welcome Ms. " + userName + "</h3>");
-
-} else {
-
-    document.write("<h3>Welcome " + userName + "</h3>");
-
-}
-
+if (userGender == "Male") {
+  document.write("welcome Mr. " + userName);
+} else if (userGender == "Femal") {
+  document.write("welcome Ms. " + userName);
+  male = false;
+} else document.write("welcome  " + userName);
 
 if (userAge >= 16) {
-
-    userChoise = prompt(
-        "Please enter one of them (Burger, Shawarma, Zinger)"
-    );
-
-    if (
-        userChoise === "Burger" ||
-        userChoise === "Shawarma" ||
-        userChoise === "Zinger"
-    ) {
-
-        alert("Your order is being prepared");
-
-        valid = true;
-
-    } else {
-
-        alert("Invalid order. Please try again");
-
-    }
-
-} else {
-
-    alert("You must be at least 16 years old to place an order");
-
+  userChoise = prompt("Please enter one of them (Burger, Shawarma , Zinger) ");
+  if (
+    userChoise == "Burger" ||
+    userChoise == "Shawarma" ||
+    userChoise == "Zinger"
+  ) {
+    alert("Your order is being prepared");
+    valid = true;
+  } else {
+    alert(".Invalid order. Please try again");
+  }
 }
 
 if (userAge >= 18 && valid) {
-
-    alert("Order confirmed");
-
-} else {
-
-    alert("Order requires verification");
-
+  alert("Order confirmed");
+} else if (userAge < 18 || !valid) {
+  alert("Order requires verification");
 }
 
-document.write("<h5>Your gender is: " + userGender + "</h5>");
+if (male) document.write("<h3>Welcome Mr. " + userName + "</h3>");
+else {
+  document.write("<h3>Welcome Ms. " + userName + "</h3>");
+}
 
-document.write("<h5>Your age is: " + userAge + "</h5>");
-
-document.write("<h5>Your order is: " + userChoise + "</h5>");
-
-document.write("<h5>Your order status is: " + valid + "</h5>");
+document.write("<h5>you gender is " + userGender + "</h5>");
+document.write("<h5>you age is " + userAge + "</h5>");
+document.write("<h5>you order is " + userChoise + "</h5>");
+document.write("<h5>you order status is " + valid + "</h5>");
